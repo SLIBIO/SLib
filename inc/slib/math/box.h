2c931b31691d9194042be338088e319ba2882af4
Fred Zhang <fred1218zh@outlook.com>
2017-03-21 02:49:45 +0800

    add license notice to source files

--- inc/slib/math/box.h
+++ inc/slib/math/box.h
@@ -1,3 +1,13 @@
+/*
+ *  Copyright (c) 2008-2017 SLIBIO. All Rights Reserved.
+ *
+ *  This file is part of the SLib.io project.
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef CHECKHEADER_SLIB_MATH_BOX
 #define CHECKHEADER_SLIB_MATH_BOX
 
--- inc/slib/render/opengl_entries.h
+++ inc/slib/render/opengl_entries.h
@@ -1,4 +1,14 @@
 /*
+ *  Copyright (c) 2008-2017 SLIBIO. All Rights Reserved.
+ *
+ *  This file is part of the SLib.io project.
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
 This file is only for internal use in the engine.
 Don't include this file directly from your own source
 */
