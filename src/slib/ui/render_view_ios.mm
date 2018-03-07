cbf3ce1db600701cc1a132e1d2b91bf1b6363cce
Fred Zhang <fred1218zh@outlook.com>
2018-03-08 03:47:50 +0800

    ios: improve OpenGL rendering

--- src/slib/ui/render_view_ios.mm
+++ src/slib/ui/render_view_ios.mm
@@ -31,6 +31,9 @@
 	id m_renderer;
 	slib::Ref<slib::RenderEngine> m_engine;
 	CADisplayLink* m_displayLink;
+	
+	CGFloat m_viewportWidth;
+	CGFloat m_viewportHeight;
 }
 
 -(void)_init;
@@ -80,6 +83,7 @@ namespace slib
 	@public __weak CADisplayLink* m_displayLink;
 	
 	BOOL m_flagRunning;
+	BOOL m_flagViewVisible;
 }
 @end
 
@@ -92,7 +96,27 @@ namespace slib
 	}
 	SLib_iOS_GLView* view = m_view;
 	if (view != nil) {
-		BOOL flagVisible = NO;
+		if (m_flagViewVisible && !(slib::MobileApp::isPaused())) {
+			if (view->m_flagRenderingContinuously || view->m_flagRequestRender) {
+				view->m_flagRequestRender = sl_false;
+				[view display];
+			}
+		}
+	}
+}
+
+- (void)queryViewState
+{
+	SLib_iOS_GLView* view = m_view;
+	if (view == nil) {
+		return;
+	}
+	BOOL flagVisible = NO;
+	sl_uint32 width = (sl_uint32)(view.frame.size.width * view.contentScaleFactor);
+	sl_uint32 height = (sl_uint32)(view.frame.size.height * view.contentScaleFactor);
+	view->m_viewportWidth = width;
+	view->m_viewportHeight = height;
+	if (width > 0 && height > 0) {
 		UIView* viewCheck = view;
 		for (;;) {
 			if (viewCheck.hidden) {
@@ -106,32 +130,33 @@ namespace slib
 				viewCheck = parent;
 			}
 		}
-		if (flagVisible && !(slib::MobileApp::isPaused())) {
-			
-			if (view->m_flagRenderingContinuously || view->m_flagRequestRender) {
-				view->m_flagRequestRender = sl_false;
-				sl_uint32 width = (sl_uint32)(view.frame.size.width * view.contentScaleFactor);
-				sl_uint32 height = (sl_uint32)(view.frame.size.height * view.contentScaleFactor);
-				if (width > 0 && height > 0) {
-					[view display];
-				}
-			}
-		}
 	}
+	m_flagViewVisible = flagVisible;
 }
 
 - (void)onRun
 {
 	m_flagRunning = YES;
+	m_flagViewVisible = NO;
 #if defined(SLIB_PLATFORM_IS_IOS_SIMULATOR)
 	slib::TimeCounter timer;
+	int frameNumber = 0;
 	while (m_flagRunning) {
+		if (frameNumber % 10 == 0) {
+			SLib_iOS_GLView* view = m_view;
+			if (view != nil) {
+				dispatch_async(dispatch_get_main_queue(), ^{
+					[self queryViewState];
+				});
+			}
+		}
 		[self onGLRenderFrame];
 		sl_uint64 t = timer.getElapsedMilliseconds();
 		if (t < 15) {
 			[NSThread sleepForTimeInterval:(15 - (sl_uint32)(t)) / 1000.0];
 		}
 		timer.reset();
+		frameNumber++;
 	}
 #else
 	NSRunLoop* loop = [NSRunLoop currentRunLoop];
@@ -153,8 +178,9 @@ namespace slib
 {
 	m_flagRenderingContinuously = sl_false;
 	m_flagRequestRender = sl_true;
+	m_viewportWidth = 0;
+	m_viewportHeight = 0;
 	
-
 	EAGLContext* context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
 	if (context == nil) {
 		context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
@@ -200,13 +226,11 @@ namespace slib
 	
 	slib::Ref<slib::View> _view = instance->getView();
 	if (slib::RenderView* view = slib::CastInstance<slib::RenderView>(_view.get())) {
-		sl_uint32 width = (sl_uint32)(self.frame.size.width * self.contentScaleFactor);
-		sl_uint32 height = (sl_uint32)(self.frame.size.height * self.contentScaleFactor);
 		if (m_engine.isNull()) {
 			m_engine = slib::GLES::createEngine();
 		}
 		if (m_engine.isNotNull()) {
-			m_engine->setViewport(0, 0, width, height);
+			m_engine->setViewport(0, 0, m_viewportWidth, m_viewportHeight);
 			view->dispatchFrame(m_engine.get());
 		}
 	}
