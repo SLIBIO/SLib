fe5325cec76c0e7cb78b76dc35fa5929c41d1033
Fred Zhang <fred1218zh@outlook.com>
2018-03-10 03:23:41 +0800

    example/CameraView: added macOS project

---
+++ examples/UI/CameraView/macOS/ExampleCameraView/AppDelegate.m
@@ -0,0 +1,20 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
---
+++ examples/UI/CameraView/macOS/ExampleCameraView/main.mm
@@ -0,0 +1,6 @@
+#include "../../src/app.h"
+
+int main(int argc, const char * argv[]) {
+	ExampleCameraViewApp::main();
+	return 0;
+}
