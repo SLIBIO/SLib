79d6be55e134820cc9dcfa6db6fd3716a9b396c9
Fred Zhang <fred1218zh@outlook.com>
2018-10-13 23:12:28 +0800

    tool/template: remove copyright notice from generated files

--- tool/template/mobile/app.cpp
+++ tool/template/mobile/app.cpp
@@ -1,13 +1,3 @@
-/*
- *  Copyright (c) 2017 SLIBIO. All Rights Reserved.
- *
- *  This file is part of the SLib.io project.
- *
- *  This Source Code Form is subject to the terms of the Mozilla Public
- *  License, v. 2.0. If a copy of the MPL was not distributed with this
- *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
 #include "app.h"
 
 SLIB_DEFINE_APPLICATION(SLIB_TEMPLATE_APP_NAMEApp, MobileApp)
