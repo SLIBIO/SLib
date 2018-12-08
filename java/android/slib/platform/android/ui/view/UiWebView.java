3e505fd348c8fc81a0b1b509579bb28432ce4378
Fred Zhang <fred1218zh@outlook.com>
2018-12-08 13:35:38 +0800

    UI: WebView: update Android alert dialog

--- java/android/slib/platform/android/ui/view/UiWebView.java
+++ java/android/slib/platform/android/ui/view/UiWebView.java
@@ -22,10 +22,13 @@
 
 package slib.platform.android.ui.view;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.os.Build;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.JsPromptResult;
@@ -34,6 +37,8 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+
 import slib.platform.android.Logger;
 import slib.platform.android.ui.UiThread;
 
@@ -337,17 +342,97 @@ public class UiWebView extends WebView implements IView {
 	}
 	
 	class MyWebChromeClient extends WebChromeClient {
-		
+
+		@Override
 		public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-			return false;
+			try {
+				final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+				builder.setTitle("Alert").setMessage(message).setPositiveButton("OK", null);
+				builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
+					public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+						return true;
+					}
+				});
+				builder.setCancelable(false);
+				AlertDialog dialog = builder.create();
+				dialog.show();
+				result.confirm();
+				return true;
+			} catch (Exception e) {
+				Logger.exception(e);
+				return false;
+			}
 		}
-		
-		public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
-			return false;
+
+		@Override
+		public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+			try {
+				final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+				builder.setTitle("Confirm")
+						.setMessage(message)
+						.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,int which) {
+								result.confirm();
+							}
+						})
+						.setNeutralButton("No", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								result.cancel();
+							}
+						});
+				builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+					@Override
+					public void onCancel(DialogInterface dialog) {
+						result.cancel();
+					}
+				});
+				builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
+					@Override
+					public boolean onKey(DialogInterface dialog, int keyCode,KeyEvent event) {
+						return true;
+					}
+				});
+				AlertDialog dialog = builder.create();
+				dialog.show();
+				return true;
+			} catch (Exception e) {
+				Logger.exception(e);
+				return false;
+			}
 		}
-		
-		public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
-			return false;
+
+		@Override
+		public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, final JsPromptResult result) {
+			try {
+				final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+				builder.setTitle("Prompt").setMessage(message);
+				final EditText et = new EditText(view.getContext());
+				et.setSingleLine();
+				et.setText(defaultValue);
+				builder.setView(et)
+						.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								result.confirm(et.getText().toString());
+							}
+
+						})
+						.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								result.cancel();
+							}
+						});
+				builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
+					public boolean onKey(DialogInterface dialog, int keyCode,KeyEvent event) {
+						return true;
+					}
+				});
+				AlertDialog dialog = builder.create();
+				dialog.show();
+				return true;
+			} catch (Exception e) {
+				Logger.exception(e);
+				return false;
+			}
 		}
 	}
 	
