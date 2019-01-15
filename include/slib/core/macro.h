384d5b328b78270e696c0020a11103ee2bb5358f
Fred Zhang <fred1218zh@outlook.com>
2019-01-15 23:25:59 +0800

    core: add variadic helper macros

--- include/slib/core/macro.h
+++ include/slib/core/macro.h
@@ -92,6 +92,17 @@
 #define SLIB_MAKE_QWORD2(A,B,C,D)			(((sl_uint64)(SLIB_MAKE_DWORD2(A,B)) << 32) | SLIB_MAKE_DWORD2(C,D))
 #define SLIB_MAKE_QWORD4(A,B)				((((sl_uint64)(sl_uint32)(A)) << 32) | (sl_uint32)(B))
 
+
+#define PRIV_SLIB_MACRO_CONCAT(A, B) A ## B
+#define SLIB_MACRO_CONCAT(A, B) PRIV_SLIB_MACRO_CONCAT(A, B)
+
+#define PRIV_SLIB_MACRO_COUNT_ARGUMENTS_HELPER(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45, a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64, ...) a64
+
+#define SLIB_MACRO_COUNT_ARGUMENTS(...) SLIB_MACRO_CONCAT(PRIV_SLIB_MACRO_COUNT_ARGUMENTS_HELPER(__VA_ARGS__, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0),)
+
+#define SLIB_MACRO_OVERLOAD(PREFIX, ...) SLIB_MACRO_CONCAT(PREFIX, SLIB_MACRO_COUNT_ARGUMENTS(__VA_ARGS__))
+
+
 #if defined(SLIB_COMPILER_IS_VC)
 #	define SLIB_HAS_FEATURE_iS_CONVERTIBLE_TO
 #elif defined(SLIB_COMPILER_IS_GCC)
