243bd4372f1e57d7fb755cd1bed7913b80a7315d
Fred Zhang <fred1218zh@outlook.com>
2019-02-14 21:38:49 +0800

    social/Facebook: compile on Android

---
+++ src/slib/social/facebook_android.cpp
@@ -0,0 +1,47 @@
+/*
+ *   Copyright (c) 2008-2019 SLIBIO <https://github.com/SLIBIO>
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+#include "slib/social/facebook.h"
+
+#if defined(SLIB_PLATFORM_IS_ANDROID)
+
+namespace slib
+{
+	
+	void Facebook::initializeOnStartApp()
+	{
+	}
+	
+	sl_bool Facebook::getCurrentToken(FacebookAccessToken* _out)
+	{
+		return sl_false;
+	}
+	
+	void Facebook::login(const FacebookLoginParam& param)
+	{
+		FacebookLoginResult result;
+		param.onComplete(result);
+	}
+	
+}
+
+#endif
