77af7d954f7f4604c148bf85caff6aac56805ddf
Fred Zhang <fred1218zh@outlook.com>
2019-12-05 15:21:35 +0800

    service/XgPushService: optimize codes

--- src/slib/service/xgpush_service.cpp
+++ src/slib/service/xgpush_service.cpp
@@ -20,8 +20,6 @@
  *   THE SOFTWARE.
  */
 
-#define SLIB_SUPPORT_STD_TYPES
-
 #include "slib/service/xgpush.h"
 
 #include "slib/network/url_request.h"
@@ -31,6 +29,42 @@
 
 namespace slib
 {
+
+	namespace priv
+	{
+		namespace xgpush
+		{
+		
+			SLIB_STATIC_STRING(g_str_title, "title")
+			SLIB_STATIC_STRING(g_str_content, "content")
+			SLIB_STATIC_STRING(g_str_body, "body")
+			SLIB_STATIC_STRING(g_str_alert, "alert")
+			SLIB_STATIC_STRING(g_str_aps, "aps")
+			SLIB_STATIC_STRING(g_str_ios, "ios")
+			SLIB_STATIC_STRING(g_str_badge_type, "badge_type")
+			SLIB_STATIC_STRING(g_str_sound, "sound")
+			SLIB_STATIC_STRING(g_str_custom, "custom")
+			SLIB_STATIC_STRING(g_str_android, "android")
+			SLIB_STATIC_STRING(g_str_ring, "ring")
+			SLIB_STATIC_STRING(g_str_ring_raw, "ring_raw")
+			SLIB_STATIC_STRING(g_str_custom_content, "custom_content")
+			SLIB_STATIC_STRING(g_str_audience_type, "audience_type")
+			SLIB_STATIC_STRING(g_str_token_list, "token_list")
+			SLIB_STATIC_STRING(g_str_platform, "platform")
+			SLIB_STATIC_STRING(g_str_environment, "environment")
+			SLIB_STATIC_STRING(g_str_message_type, "message_type")
+			SLIB_STATIC_STRING(g_str_message, "message")
+			SLIB_STATIC_STRING(g_str_notify, "notify")
+			SLIB_STATIC_STRING(g_str_token, "token")
+			SLIB_STATIC_STRING(g_str_ret_code, "ret_code")
+			SLIB_STATIC_STRING(g_str_err_msg, "err_msg")
+
+		}
+	}
+
+	using namespace priv::xgpush;
+
+	SLIB_DEFINE_CLASS_DEFAULT_MEMBERS(XgPushSendParam)
 	
 	XgPushSendParam::XgPushSendParam()
 	{
@@ -40,62 +74,59 @@ namespace slib
 	
 	void XgPushService::sendNotification(const XgPushSendParam& param)
 	{
-		HttpHeaderMap headers = {
-			{"Authorization", String::format("Basic %s", Base64::encode(String::format("%s:%s", param.appId, param.secretKey)))},
-			{"Content-Type", "application/json"}
-		};
-		Json message = Json({
-			JsonItem("title", param.message.title),
-			JsonItem("content", param.message.content)
-		});
+		HttpHeaderMap headers;
+		headers.put_NoLock(HttpHeader::Authorization, String::format("Basic %s", Base64::encode(String::format("%s:%s", param.appId, param.secretKey))));
+		headers.put_NoLock(HttpHeader::ContentType, ContentType::Json);
+		
+		Json message;
+		message.putItem(g_str_title, param.message.title);
+		message.putItem(g_str_content, param.message.content);
 		if (param.platform == XgPushPlatform::All || param.platform == XgPushPlatform::iOS) {
-			Json ios = Json({
-				JsonItem("aps", Json({
-					JsonItem("alert", Json({
-						JsonItem("title", param.message.title),
-						JsonItem("body", param.message.content),
-					})),
-				}))
-			});
+			Json alert;
+			alert.putItem(g_str_title, param.message.title);
+			alert.putItem(g_str_body, param.message.content);
+			Json aps;
+			aps.putItem(g_str_alert, alert);
 			if (param.message.badge >= 0) {
-				ios["aps"].putItem("badge_type", param.message.badge);
+				aps.putItem(g_str_badge_type, param.message.badge);
 			}
 			if (param.message.sound.isNotEmpty()) {
-				ios["aps"].putItem("sound", param.message.sound);
+				aps.putItem(g_str_sound, param.message.sound);
 			}
+			Json ios;
+			ios.putItem(g_str_aps, aps);
 			if (param.message.data.isNotNull()) {
-				ios.putItem("custom", param.message.data);
+				ios.putItem(g_str_custom, param.message.data);
 			}
-			message.putItem("ios", ios);
+			message.putItem(g_str_ios, ios);
 		}
 		if (param.platform == XgPushPlatform::All || param.platform == XgPushPlatform::Android) {
-			Json android = Json({
-				JsonItem("ring", 1),
-			});
+			Json android;
+			android.putItem(g_str_ring, 1);
 			if (param.message.sound.isNotEmpty()) {
-				android.putItem("ring_raw", param.message.sound);
+				android.putItem(g_str_ring_raw, param.message.sound);
 			}
 			if (param.message.data.isNotNull()) {
-				android.putItem("custom_content", param.message.data);
+				android.putItem(g_str_custom_content, param.message.data);
 			}
-			message.putItem("android", android);
+			message.putItem(g_str_android, android);
 		}
 		if (param.customMessage.isJsonMap()) {
 			for (auto& item : param.customMessage.getJsonMap()) {
 				message.putItem(item.key, item.value);
 			}
 		}
-		Json body = Json({
-			JsonItem("audience_type", param.receiverDeviceTokens.getCount() > 1 ? "token_list" : "token"),
-			JsonItem("token_list", param.receiverDeviceTokens),
-			JsonItem("platform", getPlatformString(param.platform)),
-			JsonItem("environment", getEnvironmentString(param.environment)),
-			JsonItem("message_type", "notify"),
-			JsonItem("message", message)
-		});
+		Json body;
+		body.putItem(g_str_audience_type, param.receiverDeviceTokens.getCount() > 1 ? g_str_token_list : g_str_token);
+		body.putItem(g_str_token_list, param.receiverDeviceTokens);
+		body.putItem(g_str_platform, getPlatformString(param.platform));
+		body.putItem(g_str_environment, getEnvironmentString(param.environment));
+		body.putItem(g_str_message_type, g_str_notify);
+		body.putItem(g_str_message, message);
 
 		auto callback = param.callback;
-		UrlRequest::post("https://openapi.xg.qq.com/v3/push/app", headers, body, [callback](UrlRequest* request) {
+		SLIB_STATIC_STRING(url, "https://openapi.xg.qq.com/v3/push/app")
+		UrlRequest::post(url, headers, body, [callback](UrlRequest* request) {
 			String response = request->getResponseContentAsString();
 			if (request->isError()) {
 				LogError("XgPush", "[Status] %d %s - Error: %s %s", (int)(request->getResponseStatus()), request->getResponseMessage(), request->getLastErrorMessage(), response);
@@ -104,14 +135,13 @@ namespace slib
 				}
 				callback(sl_false, response);
 			} else {
-				Log("XgPush", "[Status] %d %s - %s", (int)(request->getResponseStatus()), request->getResponseMessage(), response);
 				Json json = request->getResponseContentAsJson();
 				if (json.isNull()) {
 					callback(sl_false, request->getResponseContentAsString());
 				} else {
-					sl_uint32 ret_code = json["ret_code"].getUint32();
+					sl_uint32 ret_code = json[g_str_ret_code].getUint32();
 					if (ret_code != 0) {
-						callback(sl_false, json["err_msg"].getString());
+						callback(sl_false, json[g_str_err_msg].getString());
 					} else {
 						callback(sl_true, sl_null);
 					}
@@ -122,33 +152,26 @@ namespace slib
 	
 	String XgPushService::getPlatformString(XgPushPlatform platform)
 	{
-		String ret;
 		switch (platform) {
 			case XgPushPlatform::All:
-				ret = "all";
-				break;
+				SLIB_RETURN_STRING("all")
 			case XgPushPlatform::Android:
-				ret = "android";
-				break;
+				SLIB_RETURN_STRING("android")
 			case XgPushPlatform::iOS:
-				ret = "ios";
-				break;
+				SLIB_RETURN_STRING("ios")
 		}
-		return ret;
+		return sl_null;
 	}
 	
 	String XgPushService::getEnvironmentString(XgPushEnvironment environment)
 	{
-		String ret;
 		switch (environment) {
 			case XgPushEnvironment::Production:
-				ret = "product";
-				break;
+				SLIB_RETURN_STRING("product")
 			case XgPushEnvironment::Development:
-				ret = "dev";
-				break;
+				SLIB_RETURN_STRING("dev")
 		}
-		return ret;
+		return sl_null;
 	}
 	
 }
