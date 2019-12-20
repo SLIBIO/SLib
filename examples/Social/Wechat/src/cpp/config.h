f15b63eed79e56c88d2ab1abd237257582847583
Fred Zhang <fred1218zh@outlook.com>
2019-12-21 04:45:09 +0800

    social/WeChat: implement WeChat payment order

--- examples/Social/Wechat/src/cpp/config.h
+++ examples/Social/Wechat/src/cpp/config.h
@@ -6,3 +6,5 @@
 
 // Important: you should define SECRET constant on server-side only. Here, only defined for testing purpose
 #define WECHAT_APP_SECRET "YOUR_APP_SECRET"
+#define WECHAT_PAY_BUSINESS_NUMBER "YOU_WECHAT_PAY_BUSINESS_NUMBER"
+#define WECHAT_PAY_API_KEY "YOU_WECHAT_PAY_API_KEY"
